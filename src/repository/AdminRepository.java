12dc5db6508c19d6e7accab2a3912826c9bc698c
Ravi Ranjan <137057336+RaviRanjan-16@users.noreply.github.com>
2025-09-10 17:49:58 +0530

    Add files via upload

---
+++ src/repository/AdminRepository.java
@@ -0,0 +1,13 @@
+package com.hm.HospitalManagement.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hm.HospitalManagement.entity.Admin;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByEmail(String email);
+}
+
